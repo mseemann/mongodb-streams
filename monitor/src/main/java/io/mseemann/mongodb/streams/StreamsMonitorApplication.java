0e4e8f07c8a07c66e682730a92381434770516d0
mseemann <seemann@mseemann.de>
2021-09-02 21:04:47 +0200

    separate into two different spring boot apps

---
+++ monitor/src/main/java/io/mseemann/mongodb/streams/StreamsMonitorApplication.java
@@ -0,0 +1,13 @@
+package io.mseemann.mongodb.streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsMonitorApplication.class, args);
+	}
+
+}
---
+++ sync/src/test/java/io/mseemann/mongodb/streams/StreamsSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mseemann.mongodb.streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
